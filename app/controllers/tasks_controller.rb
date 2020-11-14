4f8a55a49f9ecf8c269100cad230b06336700094
kodake29 <kodake29@gmail.com>
2020-11-14 18:40:31 +0900

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,7 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
     if @task.save
+      logger.debug "task: #{@task.attributes.inspect}"
       redirect_to tasks_url, notice:"タスク「#{@task.name}」を登録しました"
     else
       render :new
@@ -31,6 +32,11 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
 
